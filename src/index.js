67270cb0932a861b3864982808ee4d7dc87b026c
Zsombor Makker <makker.zsombor@gmail.com>
2022-11-06 11:17:46 +0100

    Forms done Need invis Need to be put into list

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', () => {
-    var _a;
     // default values:
     document.getElementById('dbSzam').textContent = '0';
     document.getElementById('idbSzam').textContent = '0';
     document.getElementById('aKAra').textContent = '0';
+    // radius values
+    // error with form turning invis
+    document.getElementById('bookType').addEventListener('click', () => {
+        if (document.getElementById('pK').checked) {
+            // hide eBook
+            document.getElementById('rKonyv').textContent = '';
+            document.getElementById('eBookForm').style.visibility = 'hidden';
+            document.getElementById('pBookForm').style.visibility = 'visible';
+        }
+        else if (document.getElementById('eK').checked) {
+            // hide paperBook
+            document.getElementById('rKonyv').textContent = '';
+            document.getElementById('pBookForm').style.visibility = 'hidden';
+            document.getElementById('eBookForm').style.visibility = 'visible';
+        }
+        else {
+            document.getElementById('rKonyv').textContent = "Kérem válasszon egy könyv típust!";
+        }
+    });
     // paper check:
     document.getElementById('pKonyv').addEventListener('click', () => {
         // error hide:
@@ -18,6 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let joSuly = parseInt(document.getElementById('pSuly').value) > 0;
         let joIsbn = document.getElementById('pIsbn').value.length == 13;
         if (joKonyv && joAr && joSuly && joIsbn) {
+            // listához adás:
             console.log('papir Minden jó');
             document.getElementById('pCim').value = '';
             document.getElementById('pPrice').value = '';
@@ -28,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('rpCim').textContent = 'Nem adta meg a könyv címét!';
         }
         else if (!joAr) {
-            document.getElementById('rpPrice').textContent = 'Rossz árat adoot meg, (min 0)!';
+            document.getElementById('rpPrice').textContent = 'Rossz árat adott meg, (min 0)!';
         }
         else if (!joSuly) {
             document.getElementById('rpSuly').textContent = 'Rossz súlyt adott meg (min 1)!';
@@ -38,13 +57,36 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
     // eBook check:
-    (_a = document.getElementById('eKonyv')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+    document.getElementById('eKonyv').addEventListener('click', () => {
+        // error hide
+        document.getElementById('reCim').textContent = '';
+        document.getElementById('rePrice').textContent = '';
+        document.getElementById('reSuly').textContent = '';
+        document.getElementById('reIsbn').textContent = '';
+        // ellenörzések
         let joKonyv = document.getElementById('eCim').value.length > 0;
         let joAr = parseInt(document.getElementById('ePrice').value) >= 0;
         let joSuly = parseInt(document.getElementById('eSuly').value) > 0;
         let joIsbn = document.getElementById('eIsbn').value.length == 13;
         if (joKonyv && joAr && joSuly && joIsbn) {
-            console.log('eBook Minden jó');
+            // listához adás:
+            console.log('papir Minden jó');
+            document.getElementById('eCim').value = '';
+            document.getElementById('ePrice').value = '';
+            document.getElementById('eSuly').value = '';
+            document.getElementById('eIsbn').value = '';
+        }
+        else if (!joKonyv) {
+            document.getElementById('reCim').textContent = 'Nem adta meg a könyv címét!';
+        }
+        else if (!joAr) {
+            document.getElementById('rePrice').textContent = 'Rossz árat adott meg, (min 0)!';
+        }
+        else if (!joSuly) {
+            document.getElementById('reSuly').textContent = 'Rossz súlyt adott meg (min 1)!';
+        }
+        else if (!joIsbn) {
+            document.getElementById('reIsbn').textContent = 'Rossz ISBN-t adott meg (13 karakter)!';
         }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,25 @@ document.addEventListener('DOMContentLoaded',() => {
     (document.getElementById('idbSzam') as HTMLElement).textContent = '0';
     (document.getElementById('aKAra') as HTMLElement).textContent = '0';
 
+    // radius values
+    // error with form turning invis
+    (document.getElementById('bookType') as HTMLElement).addEventListener('click', () => {        
+        if((document.getElementById('pK') as HTMLInputElement).checked) {
+            // hide eBook
+            (document.getElementById('rKonyv') as HTMLElement).textContent = '';
+            (document.getElementById('eBookForm') as HTMLElement).style.visibility = 'hidden';
+            (document.getElementById('pBookForm') as HTMLElement).style.visibility = 'visible';
+        }else if ((document.getElementById('eK') as HTMLInputElement).checked) {
+            // hide paperBook
+            (document.getElementById('rKonyv') as HTMLElement).textContent = '';
+            (document.getElementById('pBookForm') as HTMLElement).style.visibility = 'hidden';
+            (document.getElementById('eBookForm') as HTMLElement).style.visibility = 'visible';
+        }else{
+            (document.getElementById('rKonyv') as HTMLElement).textContent = "Kérem válasszon egy könyv típust!";
+        }
+    });
+    
+
     // paper check:
     (document.getElementById('pKonyv') as HTMLElement).addEventListener('click',() => {
         // error hide:
@@ -20,6 +39,7 @@ document.addEventListener('DOMContentLoaded',() => {
         let joSuly = parseInt((document.getElementById('pSuly') as HTMLInputElement).value) > 0;
         let joIsbn = (document.getElementById('pIsbn') as HTMLInputElement).value.length == 13;
         if(joKonyv && joAr && joSuly && joIsbn){
+            // listához adás:
             console.log('papir Minden jó');            
             (document.getElementById('pCim') as HTMLInputElement).value = '';
             (document.getElementById('pPrice') as HTMLInputElement).value = '';
@@ -28,7 +48,7 @@ document.addEventListener('DOMContentLoaded',() => {
         }else if(!joKonyv){
             (document.getElementById('rpCim') as HTMLElement).textContent = 'Nem adta meg a könyv címét!';
         }else if(!joAr){
-            (document.getElementById('rpPrice') as HTMLElement).textContent = 'Rossz árat adoot meg, (min 0)!';
+            (document.getElementById('rpPrice') as HTMLElement).textContent = 'Rossz árat adott meg, (min 0)!';
         }else if(!joSuly){
             (document.getElementById('rpSuly') as HTMLElement).textContent = 'Rossz súlyt adott meg (min 1)!';
         }else if(!joIsbn){
@@ -37,13 +57,32 @@ document.addEventListener('DOMContentLoaded',() => {
     });
 
     // eBook check:
-    document.getElementById('eKonyv')?.addEventListener('click', () => {
+    (document.getElementById('eKonyv') as HTMLElement).addEventListener('click', () => {
+        // error hide
+        (document.getElementById('reCim') as HTMLInputElement).textContent = '';
+        (document.getElementById('rePrice') as HTMLInputElement).textContent = '';
+        (document.getElementById('reSuly') as HTMLInputElement).textContent = '';
+        (document.getElementById('reIsbn') as HTMLInputElement).textContent = '';
+        // ellenörzések
         let joKonyv = (document.getElementById('eCim') as HTMLInputElement).value.length > 0;
         let joAr = parseInt((document.getElementById('ePrice') as HTMLInputElement).value) >= 0;        
         let joSuly = parseInt((document.getElementById('eSuly') as HTMLInputElement).value) > 0;
         let joIsbn = (document.getElementById('eIsbn') as HTMLInputElement).value.length == 13;
         if(joKonyv && joAr && joSuly && joIsbn){
-            console.log('eBook Minden jó');
+            // listához adás:
+            console.log('papir Minden jó');            
+            (document.getElementById('eCim') as HTMLInputElement).value = '';
+            (document.getElementById('ePrice') as HTMLInputElement).value = '';
+            (document.getElementById('eSuly') as HTMLInputElement).value = '';
+            (document.getElementById('eIsbn') as HTMLInputElement).value = '';
+        }else if(!joKonyv){
+            (document.getElementById('reCim') as HTMLElement).textContent = 'Nem adta meg a könyv címét!';
+        }else if(!joAr){
+            (document.getElementById('rePrice') as HTMLElement).textContent = 'Rossz árat adott meg, (min 0)!';
+        }else if(!joSuly){
+            (document.getElementById('reSuly') as HTMLElement).textContent = 'Rossz súlyt adott meg (min 1)!';
+        }else if(!joIsbn){
+            (document.getElementById('reIsbn') as HTMLElement).textContent = 'Rossz ISBN-t adott meg (13 karakter)!';
         }
     });
 
